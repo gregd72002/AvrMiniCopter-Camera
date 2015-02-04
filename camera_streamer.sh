90801a0034f2009a637eb7b4a8691141c6d0a6a0
gregd72002 <gregd72002@gmail.com>
2015-02-04 22:14:12 +0000

    Update camera_streamer.sh

--- camera_streamer.sh
+++ camera_streamer.sh
@@ -3,7 +3,9 @@ if [ "$1" != "start" ]; then
 	echo "stoping"
 	killall raspivid
 else
-	raspivid -t 0 -b 500000 -h 480 -w 640 -fps 20 -g 20 -n -o - | gst-launch-1.0 fdsrc ! h264parse ! rtph264pay config-interval=1 pt=96 ! gdppay ! udpsink port=$3 host=$2 &
+#	raspivid -n -w 640 -h 480 -b 750000 -fps 24 -g 60 -t 0 -o - | gst-launch-1.0 fdsrc ! h264parse ! rtph264pay config-interval=10 pt=96 ! udpsink port=$3 host=$2 &
+	raspivid -n -w 640 -h 480 -b 750000 -ex sports -fps 24 -g 60 -t 0 -o - | gst-launch-1.0 fdsrc ! h264parse ! rtph264pay config-interval=10 pt=96 ! udpsink port=$3 host=$2 &
+#	raspivid -n -w 640 -h 480 -b 1000000 -ex sports -fps 24 -g 60 -t 0 -o - | gst-launch-1.0 fdsrc ! h264parse ! rtph264pay config-interval=10 pt=96 ! udpsink port=$3 host=$2 &
+#	raspivid -n -w 1280 -h 768 -b 4500000 -ex auto -fps 24 -g 60 -t 0 -o - | gst-launch-1.0 fdsrc ! h264parse ! rtph264pay config-interval=10 pt=96 ! udpsink port=$3 host=$2 &
 	echo "starting;"
 fi
-
