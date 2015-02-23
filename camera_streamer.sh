ad602e602fb4e8babdf0867225021ecdd7034623
Gregory Dymarek <gregd72002@gmail.com>
2015-02-23 20:09:28 +0000

    implementing video save

--- camera_streamer.sh
+++ camera_streamer.sh
@@ -11,11 +11,11 @@ else
 	1) raspivid -n -w 1280 -h 720 -b 4500000 -ex sports -fps 30 -g 60 -t 0 -o - | \
 		gst-launch-1.0 fdsrc ! h264parse ! rtph264pay config-interval=10 pt=96 ! udpsink port=$3 host=$2 &
 	;;
-	10) raspivid -n -w 640 -h 480 -b 2500000 -ex sports -fps 24 -g 60 -t 0 -o - | \
-		gst-launch-1.0 fdsrc ! h264parse ! tee name=t ! queue ! rtph264pay config-interval=10 pt=96 ! udpsink port=$3 host=$2 t. ! queue ! mp4mux ! filesink location=/rpicopter/VIDEO-$ts.mp4 & 
+        10) raspivid -n -w 640 -h 480 -b 2500000 -ex sports -fps 24 -g 60 -t 0 -o - | tee /rpicopter/cam/VIDEO-$ts.h264 | \
+                gst-launch-1.0 fdsrc ! h264parse ! rtph264pay config-interval=10 pt=96 ! udpsink port=$3 host=$2 &
 	;;
-	11) raspivid -n -w 1280 -h 720 -b 4500000 -ex sports -fps 30 -g 60 -t 0 -o - | \
-		gst-launch-1.0 fdsrc ! h264parse ! tee name=t ! queue ! rtph264pay config-interval=10 pt=96 ! udpsink port=$3 host=$2 t. ! queue ! mp4mux ! filesink location=/rpicopter/VIDEO-$ts.mp4 & 
+        11) raspivid -n -w 1280 -h 720 -b 4500000 -ex sports -fps 30 -g 60 -t 0 -o - | tee /rpicopter/cam/VIDEO-$ts.h264 | \
+                gst-launch-1.0 fdsrc ! h264parse ! rtph264pay config-interval=10 pt=96 ! udpsink port=$3 host=$2 &
 	;;
 	*) raspivid -n -w 640 -h 480 -b 500000 -ex sports -fps 24 -g 60 -t 0 -o - | \
 		gst-launch-1.0 fdsrc ! h264parse ! rtph264pay config-interval=10 pt=96 ! udpsink port=$3 host=$2 &
